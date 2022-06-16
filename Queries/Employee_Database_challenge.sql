256f0bbfda5edec43108f32cbee3b7dbc8005034
Gerlech <jannokusi@gmail.com>
2022-06-16 02:38:35 -0400

    Adding mentorship_eligibility CSV file to Data folder

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,3 +29,24 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+
+-- creating mentorship_eligibility table
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+-- INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON t.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+
--- schema.sql
+++ schema.sql
@@ -50,8 +50,7 @@ CREATE TABLE titles (
   title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 SELECT * FROM departments;
