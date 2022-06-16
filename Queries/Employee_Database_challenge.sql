7d958b8ca002c6c1632f5f536bb9e7e740e2956b
Gerlech <jannokusi@gmail.com>
2022-06-15 21:26:08 -0400

    Adding updated files

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,31 @@
+-- creating retirement_titles table
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+-- creating retiring_titles table
+SELECT COUNT(title) as count,
+	title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
